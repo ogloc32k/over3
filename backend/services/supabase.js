cf3b504b439da5b6015e4e2af2bca079d6f88b47
ogloc32k <ogloc32k@gmail.com>
2026-07-28 23:41:04 +0300

    Add Supabase client service

---
+++ backend/services/supabase.js
@@ -0,0 +1,7 @@
+// services/supabase.js
+const { createClient } = require('@supabase/supabase-js');
+const config = require('../config');
+
+const supabase = createClient(config.supabaseUrl, config.supabaseKey);
+
+module.exports = supabase;
