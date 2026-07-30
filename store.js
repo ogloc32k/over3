fff681958b59fd42425ea8b4d0006d8724d39cfb
ogloc32k <ogloc32k@gmail.com>
2026-07-31 00:25:22 +0300

    Refactor Store class and add TickBuffer

--- store.js
+++ store.js
@@ -1,13 +1,14 @@
 // store.js
 const EventEmitter = require('events');
 const logger = require('./logger');
+const TickBuffer = require('./engine/tickBuffer');
 
 class Store extends EventEmitter {
   constructor() {
     super();
     this.state = {
       tradingMode: 'demo',
-      balance: null,                     // ← was 10000.00, now null
+      balance: null,
       sessionPnl: 0,
       dailyPnl: 0,
       currentStake: 0.35,
@@ -19,33 +20,23 @@ class Store extends EventEmitter {
       currency: 'USD',
       marketMetrics: this._initMetrics()
     };
-    this.config = {};                    // will hold settings later
+    this.config = {};
+    this.tickBuffer = new TickBuffer(500);
   }
 
   _initMetrics() {
     const symbols = [
-      'R_10', 'R_25', 'R_50', 'R_75', 'R_100',
-      '1HZ10V', '1HZ25V', '1HZ50V', '1HZ75V', '1HZ100V'
+      'R_10','R_25','R_50','R_75','R_100',
+      '1HZ10V','1HZ25V','1HZ50V','1HZ75V','1HZ100V'
     ];
     const metrics = {};
     symbols.forEach(s => {
       metrics[s] = {
-        price: 0,
-        step: 0,
-        support: null,
-        resistance: null,
-        isBreakout: false,
-        isBreakdown: false,
-        rsi: 50,
-        volatility: 0,
-        score: 0,
-        bandwidth: 0,
-        tickDirections: [],
-        supportPct: null,
-        resistancePct: null,
-        risePct: null,
-        fallPct: null,
-        lastPrices: []
+        price: 0, step: 0, support: null, resistance: null,
+        isBreakout: false, isBreakdown: false, rsi: 50,
+        volatility: 0, score: 0, bandwidth: 0,
+        tickDirections: [], supportPct: null, resistancePct: null,
+        risePct: null, fallPct: null, lastPrices: []
       };
     });
     return metrics;
@@ -53,6 +44,12 @@ class Store extends EventEmitter {
 
   updateState(changes) {
     Object.assign(this.state, changes);
+    this.emit('stateChanged');
+  }
+
+  updateMarketMetrics(symbol, computedMetrics) {
+    if (!this.state.marketMetrics[symbol]) return;
+    this.state.marketMetrics[symbol] = { ...this.state.marketMetrics[symbol], ...computedMetrics };
     this.emit('stateChanged');
   }
 
